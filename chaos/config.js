01c93a99adc866c1656f409b5dad0d454b147a11
IWD-TG <gggrossfruitland@gmail.com>
2024-09-26 16:26:11 -0600

    added old website

---
+++ chaos/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHrrAeFXzPGjhBE3ZZjxSc9Uv19NJ3YeU",
+  authDomain: "gross-week4-day1.firebaseapp.com",
+  databaseURL: "https://gross-week4-day1-default-rtdb.firebaseio.com",
+  projectId: "gross-week4-day1",
+  storageBucket: "gross-week4-day1.appspot.com",
+  messagingSenderId: "603870795377",
+  appId: "1:603870795377:web:5f0051b8deadc48d006dd0",
+  measurementId: "G-HX2DHHQF5V"
+};
